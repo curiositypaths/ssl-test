16c23f89d34170a01b64a19cf88e180ee0fe0afb
curiositypaths <curiositypaths@users.noreply.github.com>
2018-03-22 15:07:14 -0400

    Configure webpacker and foreman

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: bundle exec puma -t 5:5 -p 23456 -e ${RACK_ENV:-development}
+webpacker: ./bin/webpack-dev-server
--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,5 +1,2 @@
 class StaticController < ApplicationController
-  def welcome
-    render inline:'Sicuro?'
-  end
 end
